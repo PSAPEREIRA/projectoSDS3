301057e7480456add7711a918c71a060d8217a9e
Pedro Sa Pereira <pedro.duarte.sapereira@gmail.com>
2021-05-05 18:58:09 +0100

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -0,0 +1,69 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+public class SellerDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	/**
+	 * 
+	 */
+	public SellerDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param id
+	 * @param name
+	 */
+	public SellerDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public SellerDTO(Seller entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+}
